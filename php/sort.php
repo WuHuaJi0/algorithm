d107a6777ac44292041fd77a59f2b7940e7a6bca
wuhuaji0 <wuhuaji0@gmail.com>
2017-12-19 13:04:30 +0800

    冒泡排序：不直接使用引用，而是返回结果

--- php/sort.php
+++ php/sort.php
@@ -6,16 +6,15 @@ function swap(&$a,&$b) {
     $b = $tmp;
 }
 
-function bubble_sort(&$arr){
+function bubble_sort($arr){
     for($i = 0; $i < count($arr) -1; $i++){
-        echo $i;
         for($j = 0;$j < count($arr) -1 - $i ;$j++){
-            echo $j;
             if ($arr[$j] > $arr[$j + 1]){
                 swap($arr[$j],$arr[$j + 1]);
             }
         }
     }
+    return $arr;
 }
 
 /**冒泡排序结束*/
