7fe5f7412add31d567d69a651a3b8bba7e791402
wuhuaji0 <wuhuaji0@gmail.com>
2017-12-19 13:04:57 +0800

    构建简单测试方法，测试冒泡排序

---
+++ php/sort_test.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wuhuaji
+ * Date: 19/12/2017
+ * Time: 11:03 AM
+ */
+
+require_once 'sort.php';
+
+/**一个简单测试函数，用于测试排序后结果是否相等*/
+function testEqual(&$origin,$sorted,$sortfunc){
+    $origin = call_user_func($sortfunc,$origin);
+    if ($sorted == $origin){
+        echo $sortfunc." 成功\n";
+    }else{
+        echo $sortfunc." 失败\n";
+    }
+}
+
+/**冒泡排序测试*/
+$origin = [2,3,5,1,0,6,-9];
+$sorted = [-9,0,1,2,3,5,6];
+testEqual($origin,$sorted,"bubble_sort");
