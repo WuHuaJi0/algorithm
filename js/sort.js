6b9a7965d8497d4ff825f18eef68d6033ffce983
wuhuaji0 <wuhuaji0@gmail.com>
2017-12-24 20:11:04 +0800

    一个符号的错误

--- js/sort.js
+++ js/sort.js
@@ -3,8 +3,8 @@
 /**冒泡排序*/
 function bubble_sort(arr){
     for(var i = 0 ;i<arr.length - 1;i++){
-        for(var j = 0;j<arr.length -1 - i;j++){
-            if (arr[j] > arr[j-1]){
+        for(var j = 0;j<arr.length - 1 - i ;j++){
+            if (arr[j] > arr[j+1]){
                 var tmp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j + 1] = tmp;
