fc0f306812182f779e5a6e270dddaba7f45e53c7
wuhuaji0 <wuhuaji0@gmail.com>
2017-12-24 20:12:06 +0800

    编写JS的排序测试，测试冒泡排序

---
+++ js/sort_test.js
@@ -0,0 +1,21 @@
+/**
+ * Created by wuhuaji on 19/12/2017.
+ */
+var sort = require('./sort')
+
+function testEqual(origin,sorted,func){
+    var result = func(origin);
+    console.log(result)
+    console.log(sorted)
+    for(var i = 0;i<result.length;i++){
+        if (result[i] !== sorted[i]){
+            console.log("排序失败");
+            return false;
+        }
+    }
+    console.log("排序成功")
+}
+
+var origin = [2,3,5,1,0,6,-9];
+var sorted = [-9,0,1,2,3,5,6];
+testEqual(origin,sorted,sort.bubble_sort);
